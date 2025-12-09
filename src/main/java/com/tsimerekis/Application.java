0bad8bd894e4063c1355ecdb0a164045d1a22086
Nicholas Tsimerekis <ntsimerekis@protonmail.com>
2025-12-08 20:38:00 -0800

    Spring set up
    Re-allow Spring Security and set logging to debug.

--- src/main/java/com/tsimerekis/Application.java
+++ src/main/java/com/tsimerekis/Application.java
@@ -6,7 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication
 @Theme("default")
 public class Application implements AppShellConfigurator {
 
