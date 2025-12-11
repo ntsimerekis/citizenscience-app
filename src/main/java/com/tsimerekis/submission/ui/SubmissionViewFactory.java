31e878d7430a299546fa9a34d1c74ef8586c8122
Nicholas Tsimerekis <ntsimerekis@protonmail.com>
2025-12-10 19:38:59 -0800

    added non-functioning map
    I think there is a bug thats not allowing the blips to show

--- src/main/java/com/tsimerekis/submission/ui/SubmissionViewFactory.java
+++ src/main/java/com/tsimerekis/submission/ui/SubmissionViewFactory.java
@@ -1,13 +1,18 @@
 package com.tsimerekis.submission.ui;
 
+import com.tsimerekis.geometry.GeometryHelper;
 import com.tsimerekis.submission.entity.PollutionReport;
 import com.tsimerekis.submission.entity.SpeciesSpotting;
 import com.tsimerekis.submission.entity.Submission;
 import com.tsimerekis.submission.exception.InvalidSubmissionException;
 import com.tsimerekis.submission.exception.MissingSpeciesException;
 import com.tsimerekis.submission.service.SubmissionService;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.map.Map;
+import com.vaadin.flow.component.map.configuration.Coordinate;
+import com.vaadin.flow.component.map.configuration.feature.MarkerFeature;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -37,6 +42,7 @@ public class SubmissionViewFactory {
         final VerticalLayout form = new VerticalLayout();
 
         if (submission instanceof PollutionReport pollutionReport) {
+
             final PollutionReportView pollutionReportView = new PollutionReportView(pollutionReport);
             pollutionReportView.allowWrite();
 
@@ -77,11 +83,45 @@ public class SubmissionViewFactory {
                 Notification.show("Submitted");
                 dialog.close();
             }));
-
-            return speciesSubmissionView;
         }
+        addLocationSelectorWidget(form, submission);
 
         return form;
     }
 
+    private void addLocationSelectorWidget(VerticalLayout form, Submission submission) {
+        final Map map = new Map();
+
+        // Set Center and Zoom to ensure the marker is visible
+        map.setCenter(new Coordinate(33.261657, -79.983697));
+        map.setZoom(12);
+
+        // Define the initial coordinates for the marker
+        Coordinate initialCoordinate = new Coordinate(33.261657, -79.983697);
+
+        final MarkerFeature marker = new MarkerFeature(initialCoordinate);
+        marker.setId("draggable-marker");
+        marker.setDraggable(true);
+        marker.setText("Drag me");
+        map.getFeatureLayer().addFeature(marker);
+
+        map.setWidth(512, Unit.PIXELS);
+        map.setHeight(512, Unit.PIXELS);
+
+        // ... (rest of the code for click/drop listeners is fine)
+        map.addClickEventListener(event -> {
+            final MarkerFeature newMarker = new MarkerFeature(event.getCoordinate());
+            map.getFeatureLayer().addFeature(newMarker);
+        });
+
+        map.addFeatureDropListener(event -> {
+            MarkerFeature droppedMarker = (MarkerFeature) event.getFeature();
+            // ... (rest of the drop logic)
+            Coordinate endCoordinates = event.getCoordinate();
+            submission.setLocation(GeometryHelper.getJTSPoint(endCoordinates));
+            Notification.show("Marker \"" + droppedMarker.getId() + "\" dragged to " + endCoordinates);
+        });
+
+        form.add(map);
+    }
 }
